ec3d0f9960f21f59de341cc81b9ffb0d2f7728c6
unknown <Alejo Gonzalez>
2024-11-29 01:03:39 -0300

    front - quitado el filtrado de Marcas por descripcion

--- frontend/src/components/Marca/FiltroMarcas.js
+++ frontend/src/components/Marca/FiltroMarcas.js
@@ -3,7 +3,6 @@ import { Form, Row, Col } from 'react-bootstrap';
 
 const FiltroMarcas = ({ 
     nombreFilter, setNombreFilter,
-    descripcionFilter, setDescripcionFilter
 }) => {
     return (
         <Form>
@@ -16,17 +15,6 @@ const FiltroMarcas = ({
                             placeholder="Buscar por nombre de marca" 
                             value={nombreFilter} 
                             onChange={e => setNombreFilter(e.target.value)} 
-                        />
-                    </Form.Group>
-                </Col>
-                <Col>
-                    <Form.Group controlId="descripcionFilter">
-                        <Form.Label>Descripción</Form.Label>
-                        <Form.Control 
-                            type="text" 
-                            placeholder="Buscar por descripción" 
-                            value={descripcionFilter} 
-                            onChange={e => setDescripcionFilter(e.target.value)} 
                         />
                     </Form.Group>
                 </Col>
